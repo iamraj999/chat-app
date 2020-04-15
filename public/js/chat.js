442a5a52c395f64c0a31238bf9fa4cdff3f0fc9c
R Nandyala <shekar2906@gmail.com>
2020-04-14 17:40:46 -0700

    rendering messages to browser

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,38 @@
 const socket = io();
 
-socket.on('message', (msg) => {
-    console.log(msg)
+const form = document.querySelector('#msgForm');
+const msg = document.querySelector('#usermsg');
+const button = form.querySelector('button');
+const messages = document.querySelector('#messages');
+
+//templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message
+    });
+    messages.insertAdjacentHTML('beforebegin', html);
 });
 
-const form = document.querySelector('#msgForm');
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate, {
+        url
+    });
+    messages.insertAdjacentHTML('beforebegin', html);
+})
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const msg = document.querySelector('#usermsg');
+    button.setAttribute('disabled', 'disabled')
     socket.emit('sendMessage', msg.value, (error) => {
+        button.removeAttribute('disabled');
+        button.focus();
         if (error) {
             return console.log(error)
         }
@@ -22,11 +45,13 @@ const locationButton = document.querySelector('#send-location');
 
 locationButton.addEventListener('click', () => {
     if (navigator.geolocation) {
+        locationButton.setAttribute('disabled', 'disabled')
         navigator.geolocation.getCurrentPosition((position) => {
             socket.emit('sendLocation', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             }, () => {
+                locationButton.removeAttribute('disabled')
                 console.log('location shared')
             })
 
--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
         callback('delivered!');
     })
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         callback();
     })
     socket.on('disconnect', () => {
